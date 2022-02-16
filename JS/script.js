24f9d779c6c7fe2b4fb62389695fc2ef5c9aab96
Ruben Åkenes <ruben.akenes@gmail.com>
2022-02-16 11:15:51 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector('.toggle')
+                                      
+const navigation = document.querySelector('.navigation')
+                                      
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
+
+
+                       
+                        
